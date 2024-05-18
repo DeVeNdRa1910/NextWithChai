7a01afc4ed65ddd182819c6c37c9f543df23f3b8
Devendra <devendrav1910@gmail.com>
2024-05-18 17:12:24 +0530

    till Cards

--- projectone/src/app/page.tsx
+++ projectone/src/app/page.tsx
@@ -1,7 +1,12 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-center">Hello Devendra welcome to Next Js world</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+    </main>
   );
 }
--- projectone/tailwind.config.ts
+++ projectone/tailwind.config.ts
@@ -1,4 +1,23 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+
+// Plugin to add each Tailwind color as a global CSS variable
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  );
+
+  addBase({
+    ':root': newVars,
+  });
+}
 
 const config: Config = {
   content: [
@@ -6,15 +25,23 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },      
+      keyframes: {
+        spotlight: {
+          "0%": { opacity: '0',transform: "translate(-72%, -62%) scale(0.5)",},
+          "100%": { opacity: '1',transform: "translate(-50%,-40%) scale(1)",},
+        },
       },
     },
+    plugins: [addVariablesForColors],
   },
-  plugins: [],
 };
+
+
+
 export default config;
