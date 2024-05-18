45f03ce8038d5f3ca6bdf68844458e640881bddf
Devendra <devendrav1910@gmail.com>
2024-05-18 11:31:54 +0530

    start journey with NEXT and chai

---
+++ projectone/src/app/music/love.tsx
@@ -0,0 +1,5 @@
+export default function(){
+  return(
+    <h1>Hello Next JS I am devendra</h1>
+  )
+}
---
+++ projectone/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold text-center">Hello Devendra welcome to Next Js world</h1>
+  );
+}
